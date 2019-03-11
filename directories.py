9957bf83883c9b2bd41aa8916166c3e5347c8409
Ruggero Basanisi <ruggero.basanisi@gmail.com>
2019-03-11 12:29:18 +0100

    solved separators problems for win/unix compatibility

--- directories.py
+++ directories.py
@@ -1,11 +1,13 @@
 # In this script is possible to modify the folder in which the data are stored.
+import os
+s = os.sep
 
-directory = 'D:\\Databases\\db_lfp\\meg_causal\\'
+directory = 'D:'+s+'Databases'+s+'db_lfp'+s+'meg_causal'+s
 xls_fname = 'dataset_StriPAN_proba-{0}.xlsx'
-rawmat_dir = directory + '{0}\\{1}\\raw_matlab\\' #{2}.mat' #.format(subject, condition, session)
-info_dir = directory + '{0}\\{1}\\infos\\' #info_{2}.json' #.format(subject, condition, session)
-neu_dir = directory + '{0}\\{1}\\neu_data\\' #{2}.npz' #.format(subject, condition, session)
-beh_dir = directory + '{0}\\{1}\\beh_data\\' #{2}.npz' #.format(subject, condition, session)
-raw_dir = directory + '{0}\\{1}\\raw\\'
-epochs_dir = directory + '{0}\\{1}\\epochs\\'
+rawmat_dir = directory + '{0}'+s+'{1}'+s+'raw_matlab'+s #{2}.mat' #.format(subject, condition, session)
+info_dir = directory + '{0}'+s+'{1}'+s+'infos'+s #info_{2}.json' #.format(subject, condition, session)
+neu_dir = directory + '{0}'+s+'{1}'+s+'neu_data'+s #{2}.npz' #.format(subject, condition, session)
+beh_dir = directory + '{0}'+s+'{1}'+s+'beh_data'+s #{2}.npz' #.format(subject, condition, session)
+raw_dir = directory + '{0}'+s+'{1}'+s+'raw'+s
+epochs_dir = directory + '{0}'+s+'{1}'+s+'epochs'+s
 # prep_dir = directory + '{0}\\prep\\{1}\\{0}_outcome-epo.fif'
