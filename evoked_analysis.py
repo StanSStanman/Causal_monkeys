ab6f8f7f4fb3fcfb17ea46f6a49344d717a5bd12
Ruggero Basanisi <ruggero.basanisi@gmail.com>
2019-03-13 11:43:44 +0100

    script for evoked analysis aligned on cue and trigger

--- evoked_analysis.py
+++ evoked_analysis.py
@@ -17,6 +17,13 @@ def collect_evoked(subject, condition, session, item='trigger', picks=None):
     cue_epochs = epochs.copy().crop(cue_interval[0], cue_interval[1])
     trigger_epochs = epochs.copy().crop(trigger_interval[0], trigger_interval[1])
 
+    cue_evoked = cue_epochs.copy().average()
+    cue_sem = cue_epochs.copy().standard_error()
+    trigger_evoked = trigger_epochs.copy().average()
+    trigger_sem = trigger_epochs.copy().standard_error()
+
+    
+
 
 if __name__ == '__main__':
     collect_evoked('freddie', 'easy', 'fneu0437', picks=['lfp'])
